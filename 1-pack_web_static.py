b3604183e2b589cc076b044230db3a5edaad8c00
Sirius1616 <siriusa1.615@gmail.com>
2024-07-13 14:10:54 -0700

    web stack deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,17 +4,19 @@ from datetime import datetime
 
 def do_pack():
     """A script that creates an zip archive for packing up files to be deployed"""
-    time_format = datetime.now().strftime("%Y%m%d%H%M%S")
-
+    
     try:
         local("mkdir -p versions")
+
+        time_format = datetime.now().strftime("%Y%m%d%H%M%S")
         
-        file_name = "versions/web_static_{}.tgz web_static/".format(time_format)
+        file_name = "versions/web_static_{}.tgz".format(time_format)
         local("tar -czvf {} web_static".format(file_name))
         
         return file_name
 
     except Exception as e:
+        print(e)
         return None
 
 
