3bebcb11ba923239f8268977954cc16df1aece61
Sirius1616 <siriusa1.615@gmail.com>
2024-07-13 15:18:15 -0700

    web stack deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,18 +7,14 @@ def do_pack():
     """A script that creates an zip archive for packing up files to be deployed"""
     
     try:
+        local("mkdir -p versions")
 
         time_format = strftime("%Y%m%d%H%M%S")
-        local("mkdir -p versions")
+        
+        file_name = "versions/web_static_{}.tgz".format(time_format)
         local("tar -czvf {} web_static".format(file_name))
         
-        return "versions/web_static_{}.tgz".format(time_format)
+        return file_name
 
     except Exception as e:
         return None
-
-
-
-
-
-
