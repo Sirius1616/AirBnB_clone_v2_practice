b67c9dd563d18e93ea4debbceb0f33c6f69dd127
Sirius1616 <siriusa1.615@gmail.com>
2024-07-13 15:25:01 -0700

    web stack deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,10 +6,11 @@ from time import strftime
 def do_pack():
     """A script that creates an zip archive for packing up files to be deployed"""
     
+    time_format = strftime("%Y%m%d%H%M%S")
+
     try:
         local("mkdir -p versions")
 
-        time_format = strftime("%Y%m%d%H%M%S")
         
         file_name = "versions/web_static_{}.tgz".format(time_format)
         local("tar -czvf {} web_static".format(file_name))
